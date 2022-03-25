e095c0a4471dbb30e788ff9120be48f0f0bbfeaf
Luis Amorim <luisfelipeamorim@hotmail.com>
2022-03-25 19:27:20 -0300

    feat(commissions): added base files for commission module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommissionController } from './commission/commission.controller';
+import { CommissionService } from './commission/commission.service';
+import { CommissionModule } from './commission/commission.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CommissionModule],
+  controllers: [AppController, CommissionController],
+  providers: [AppService, CommissionService],
 })
 export class AppModule {}
