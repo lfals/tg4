31aa24e8d2af09ee9d1fa149fd678146aad46d59
Luis Amorim <luisfelipeamorim@hotmail.com>
2022-03-27 01:00:04 -0300

    test(commission calculator test): added jest testing for Commission calculator

--- src/commission/commission.controller.spec.ts
+++ src/commission/commission.controller.spec.ts
@@ -1,18 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommissionController } from './commission.controller';
+import { CommissionService } from './commission.service';
 
 describe('CommissionController', () => {
-  let controller: CommissionController;
+  let commissionController: CommissionController;
+  let commissionService: CommissionService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [CommissionController],
-    }).compile();
-
-    controller = module.get<CommissionController>(CommissionController);
+  beforeEach(() => {
+    commissionService = new CommissionService();
+    commissionController = new CommissionController(commissionService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('Commission calculator', () => {
+    it('Vendedor deve receber bonus se atingir a meta', () => {
+      const fakeInput = {
+        pedidos: [
+          { vendedor: 1, data: '2022-03-01', valor: 100.0 },
+          { vendedor: 1, data: '2022-03-01', valor: 100.0 },
+          { vendedor: 1, data: '2022-03-01', valor: 100.0 },
+        ],
+      };
+      const result = commissionController.commissions(fakeInput);
+      expect(result).toEqual([
+        {
+          vendedor: 1,
+          mes: '03',
+          valor: '3.09',
+        },
+      ]);
+    });
+
+    it('Vendedor não deve receber bonus se atingir a meta', () => {
+      const fakeInput = {
+        pedidos: [{ vendedor: 1, data: '2022-03-01', valor: 100.0 }],
+      };
+      const result = commissionController.commissions(fakeInput);
+      expect(result).toEqual([
+        {
+          vendedor: 1,
+          mes: '03',
+          valor: '1.00',
+        },
+      ]);
+    });
+
+    it('Vendedor deve receber comissao segundo a faixa', () => {
+      const fakeInput = {
+        pedidos: [{ vendedor: 1, data: '2022-03-01', valor: 1000.0 }],
+      };
+      const result = commissionController.commissions(fakeInput);
+      expect(result).toEqual([
+        {
+          vendedor: 1,
+          mes: '03',
+          valor: '30.00',
+        },
+      ]);
+    });
   });
 });
